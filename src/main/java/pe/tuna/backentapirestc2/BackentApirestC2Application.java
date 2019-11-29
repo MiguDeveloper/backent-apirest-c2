81db499355c5d7495d3f1b930557c4d45c9f0dd0
MiguDeveloper <miguel.peru.seo@gmail.com>
2019-11-28 22:25:27 -0500

    configuracion app backend-rest

---
+++ src/main/java/pe/tuna/backentapirestc2/BackentApirestC2Application.java
@@ -0,0 +1,13 @@
+package pe.tuna.backentapirestc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackentApirestC2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackentApirestC2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pe/tuna/backentapirestc2/BackentApirestC2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.tuna.backentapirestc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackentApirestC2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
